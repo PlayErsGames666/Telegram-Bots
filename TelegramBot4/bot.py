28852dd240190bb8569b92576489d38c26b997da
PlayErsGames666 <playersgames.ru@gmail.com>
2026-02-17 17:56:26 +0500

    Bot with buttons, which i will develop after.

---
+++ TelegramBot4/bot.py
@@ -0,0 +1,31 @@
+import telebot
+import config
+
+from config import TOKEN
+from telebot import types
+
+
+bot=telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,'Привет')
+
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Кнопка 2")
+        markup.add(item1)
+        bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+    elif message.text=="Кнопка 2":
+        bot.send_message(message.chat.id,'Спасибо за прочтение статьи!')
+
+bot.polling(non_stop=True)
